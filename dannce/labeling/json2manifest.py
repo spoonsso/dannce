4b32f75e2dadb3a4ec771106f2198f5aff0ca840
diegoaldarondo <diegoaldarondo@g.harvard.edu>
2019-06-21 12:32:24 -0400

    Finished linting python modules.

--- dannce/labeling/json2manifest.py
+++ dannce/labeling/json2manifest.py
@@ -1,22 +1,19 @@
-"""
-Takes the json files with image ID onformation provided by SageMaker, and creates more readble manifest files.
-This is a required first step before consolidating manual annotations
+"""Create manifest files from json files.
 
+Takes the json files with image ID onformation provided by SageMaker,
+and creates more readble manifest files. This is a required first step before
+consolidating manual annotations
 """
-
-import numpy
 import json
-import sys
+
 
 def j2m(json_file):
+	"""Convert json to manifest."""
 	man_file = json_file.split('.json')[0] + '.manifest'
-
-	with open(man_file,'w') as g:
-		with open(json_file,'r') as f:
+	with open(man_file, 'w') as g:
+		with open(json_file, 'r') as f:
 			for line in f:
 				x = json.loads(line)
 				ID = x['datasetObjectId']
 				imname = json.loads(x['manifestLine'])['source-ref']
 				g.write('{}\t{}\t0\n'.format(ID, imname))
-
-
