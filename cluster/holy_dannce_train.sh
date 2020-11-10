98239eafd3d5df02af4e1fd3eb7189e3e206609b
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2020-11-10 18:02:56 -0500

    modify submission params for all scripts
    Former-commit-id: f454453cf3dbcf5427c4ae9523c1d30434465204

--- cluster/holy_dannce_train.sh
+++ cluster/holy_dannce_train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=trainDannce
 # Job name
-#SBATCH --mem=80000
+#SBATCH --mem=60000
 # Job memory request
 #SBATCH -t 2-00:00
-# Time limit hrs:min:secS
+# Time limit hrs:min:sec
 #SBATCH -N 1
-#SBATCH -n 16
+#SBATCH -c 16
 #SBATCH -p olveczkygpu,gpu
 #SBATCH --gres=gpu:1
 module load Anaconda3/5.0.1-fasrc02
