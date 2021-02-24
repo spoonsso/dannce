6518415fc723de51b766664bd8f2ec3793dc9af8
Timothy Dunn <dunn.tw@gmail.com>
2021-02-24 15:14:13 -0500

    updated dannce version number in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dannce",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "six",
