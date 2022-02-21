fix rotation augmentation, outputs for temporal, memory for sil load

dannce/engine/generator.py
@@ -817,7 +817,9 @@ class DataGenerator_3Dconv_frommem(keras.utils.Sequence):
         self.heatmap_reg_coeff = heatmap_reg_coeff
         self.aux_labels = aux_labels
         self.temporal_chunk_list = temporal_chunk_list
-        self._get_temporal_batch_size()
+        self.temporal_chunk_size = 1
+        self.temporal_batch_size = batch_size
+        self._update_temporal_batch_size()
         self.on_epoch_end()
 
     def __len__(self):
@@ -831,9 +833,10 @@ class DataGenerator_3Dconv_frommem(keras.utils.Sequence):
 
         return len(self.list_IDs) // self.batch_size
     
-    def _get_temporal_batch_size(self):
+    def _update_temporal_batch_size(self):
         if self.temporal_chunk_list is not None:
-            self.temporal_batch_size = self.batch_size // len(self.temporal_chunk_list[0])
+            self.temporal_chunk_size = len(self.temporal_chunk_list[0])
+            self.temporal_batch_size = self.batch_size // self.temporal_chunk_size
 
     def __getitem__(self, index):
         """Generate one batch of data.
@@ -914,36 +917,33 @@ class DataGenerator_3Dconv_frommem(keras.utils.Sequence):
             X (np.ndarray): Rotated image volumes
             y_3d (np.ndarray): Rotated 3D grid coordinates (AVG) or training target volumes (MAX)
         """
-        rots = np.random.choice(np.arange(4), X.shape[0])
-        for i in range(X.shape[0]):
+        # if use temporal, rotation augmentations have to be consistent within each temporal chunks
+        # for example, assume the temporal chunk size to be 2, each batch contains exactly 2 chunks
+        # then, only 2 different rotation schemes need to be generated
+        rots = np.random.choice(np.arange(4), self.temporal_batch_size)
+        for i in range(0, self.temporal_chunk_size, self.batch_size):
             if rots[i] == 0:
                 pass
             elif rots[i] == 1:
                 # Rotate180
-                X[i] = self.rot180(X[i])
-                y_3d[i] = self.rot180(y_3d[i])
-                if aux is not None:
-                    aux[i] = self.rot180(aux[i])
+                for j in range(i, i+self.temporal_chunk_size):
+                    X[j], y_3d[j] = self.rot180(X[j]), self.rot180(y_3d[i])
+                    if aux is not None:
+                        aux[i] = self.rot180(aux[i])
             elif rots[i] == 2:
                 # Rotate90
-                X[i] = self.rot90(X[i])
-                y_3d[i] = self.rot90(y_3d[i])
-                if aux is not None:
-                    aux[i] = self.rot90(aux[i])
+                for j in range(i, i+self.temporal_chunk_size):
+                    X[j], y_3d[j] = self.rot90(X[j]), self.rot90(y_3d[i])
+                    if aux is not None:
+                        aux[i] = self.rot90(aux[i])
             elif rots[i] == 3:
                 # Rotate -90/270
-                X[i] = self.rot90(X[i])
-                X[i] = self.rot180(X[i])
-                y_3d[i] = self.rot90(y_3d[i])
-                y_3d[i] = self.rot180(y_3d[i])
-                if aux is not None:
-                    aux[i] = self.rot90(aux[i])
-                    aux[i] = self.rot180(aux[i])
+                for j in range(i, i+self.temporal_chunk_size):
+                    X[j], y_3d[j] = self.rot180(self.rot90(X[j])), self.rot180(self.rot90(y_3d[i]))
+                    if aux is not None:
+                        aux[i] = self.rot180(self.rot90(aux[i]))
 
-        if aux is not None:
-            return X, y_3d, aux
-        else:
-            return X, y_3d
+        return X, y_3d, aux
 
     def visualize(self, original, augmented):
         """Plots example image after augmentation
@@ -989,7 +989,7 @@ class DataGenerator_3Dconv_frommem(keras.utils.Sequence):
                 if aux is not None:
                     X, X_grid, aux = self.random_rotate(X.copy(), X_grid.copy(), aux.copy())
                 else:
-                    X, X_grid = self.random_rotate(X.copy(), X_grid.copy())
+                    X, X_grid, aux = self.random_rotate(X.copy(), X_grid.copy(), aux)
                 # Need to reshape back to raveled version
                 X_grid = np.reshape(X_grid, (self.batch_size, -1, 3))
             else:
@@ -1137,6 +1137,7 @@ class DataGenerator_3Dconv_frommem(keras.utils.Sequence):
         # Randomly re-order, if desired
         X = self.do_random(X)
 
+        # adjust inputs & outputs according to targeted losses
         return_input, return_target = [X], [y_3d]
         if not self.expval:
             return return_input, return_target

dannce/engine/processing.py
@@ -1457,46 +1457,86 @@ def write_npy(uri, gen):
             np.save(os.path.join(imdir, fname + ".npy"), bch[0][0][j].astype("uint8"))
             np.save(os.path.join(griddir, fname + ".npy"), bch[0][1][j])
 
+def extract_3d_sil(vol, upper_thres):
+    vol[vol > 0] = 1
+    vol = np.sum(vol, axis=-1, keepdims=True)
+
+    vol[vol < upper_thres] = 0
+    vol[vol > 0] = 1
+
+    print("{}\% of silhouette training voxels are occupied".format(
+            100*np.sum(vol)/len(vol.ravel())))
+    return vol
+
 def load_volumes_into_mem(params, partition, n_cams, generator, train=True, silhouette=False):
     n_samples = len(partition["train_sampleIDs"]) if train else len(partition["valid_sampleIDs"]) 
     message = "Loading training data into memory" if train else "Loading validation data into memory"
     gridsize = tuple([params["nvox"]] * 3)
 
-    X = np.zeros((n_samples, *gridsize, params["chan_num"]*n_cams), dtype="float32")
+    X = np.empty((n_samples, *gridsize, params["chan_num"]*n_cams), dtype="float32")
     print(message)
 
     X_grid = None
     if params["expval"]:
-        y = np.zeros((n_samples, 3, params["new_n_channels_out"]), dtype="float32")
-        X_grid = np.zeros((n_samples, params["nvox"] ** 3, 3), dtype="float32")
+        if not silhouette: 
+            y = np.empty((n_samples, 3, params["new_n_channels_out"]), dtype="float32")
+            X_grid = np.empty((n_samples, params["nvox"] ** 3, 3), dtype="float32")
     else:
-        y = np.zeros((n_samples, *gridsize, params["new_n_channels_out"]), dtype="float32")
+        y = np.empty((n_samples, *gridsize, params["new_n_channels_out"]), dtype="float32")
 
     for i in range(n_samples):
         print(i, end="\r")
         rr = generator.__getitem__(i)
         if params["expval"]:
             X[i] = rr[0][0]
-            X_grid[i] = rr[0][1]
+            if not silhouette: 
+                X_grid[i], y[i] = rr[0][1], rr[1]
         else:
-            X[i] = rr[0]
-        y[i] = rr[1]
+            X[i], y[i] = rr[0], rr[1]
 
     if silhouette:
         print("Now loading silhouettes")
-        def extract_3d_sil(vol):
-            vol[vol > 0] = 1
-            vol = np.sum(vol, axis=-1, keepdims=True)
-
-            vol[vol < (params["chan_num"] * n_cams)] = 0
-            vol[vol > 0] = 1
-
-            print("{}\% of silhouette training voxels are occupied".format(
-                    100*np.sum(vol)/len(vol.ravel())))
-            return vol
     
-        X = extract_3d_sil(X)
+        X = extract_3d_sil(X, params["chan_num"]*n_cams)
         return None, None, X
     
     return X, X_grid, y
+
+# def load_volumes_into_mem(params, partition, n_cams, generator, train=True, silhouette=False):
+#     n_samples = len(partition["train_sampleIDs"]) if train else len(partition["valid_sampleIDs"]) 
+#     message = "Loading training data into memory" if train else "Loading validation data into memory"
+
+#     X = []
+#     print(message)
+
+#     y = []
+#     X_grid = None
+#     if params["expval"]:
+#         if not silhouette: 
+#             X_grid = []
+
+#     for i in range(n_samples):
+#         print(i, end="\r")
+#         rr = generator.__getitem__(i)
+#         if params["expval"]:
+#             X.append(rr[0][0])
+#             if not silhouette: 
+#                 X_grid.append(rr[0][1])
+#                 y.append(rr[1])
+#         else:
+#             X.append(rr[0])
+#             y.append(rr[1])
+
+#     X = np.concatenate(X, axis=0)
+#     if silhouette:
+#         print("Now loading silhouettes")
+    
+#         X = extract_3d_sil(X, params["chan_num"]*n_cams)
+#         return None, None, X
+
+#     if X_grid is not None:
+#         X_grid = np.concatenate(X_grid, axis=0)
+#     y = np.concatenate(y, axis=0)
+    
+#     return X, X_grid, y
 